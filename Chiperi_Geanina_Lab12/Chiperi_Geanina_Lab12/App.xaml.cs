43beea06f38d4e201e6df05144f902b7a673bd6b
Geanina <chiperi.geanina@gmail.com>
2021-01-10 16:36:53 +0200

    Add project files.

---
+++ Chiperi_Geanina_Lab12/Chiperi_Geanina_Lab12/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Chiperi_Geanina_Lab12;
+
+namespace Chiperi_Geanina_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Chiperi_Geanina_Lab12/Chiperi_Geanina_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Chiperi_Geanina_Lab12.Models;
+
+namespace Chiperi_Geanina_Lab12.Data
+{
+    public interface IRestService
+    {
+            Task<List<ShopList>> RefreshDataAsync();
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id);
+    }
+}
